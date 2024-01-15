19d1364e6abf11f7ba47ee6fd8446a049a36f8d6
TimoEichelmann <teichelm@student.42berlin.de>
2024-01-15 21:27:13 +0100

    i_love_lousi

--- fdf.c
+++ fdf.c
@@ -323,7 +323,7 @@ t_point **ft_initialize(int fd)
 	return (grid);
 }
 
-void	ft_free(t_point **grid)
+void	ft_free(t_point **grid, t_data *img, void *mlx, void *mlx_win)
 {
 	int	i;
 	int	j;
@@ -342,6 +342,59 @@ void	ft_free(t_point **grid)
 		i++;
 		j = 0;
 	}
+	mlx_destroy_image(mlx, img);
+	mlx_clear_window(mlx, mlx_win);
+	mlx_destroy_window (mlx, mlx_win);
+}
+
+void	ft_draw_grid(t_point **grid)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (grid[i])
+	{
+		while (grid[i][j].end)
+		{
+			if (grid[i][j + 1].end)
+				ft_draw_line(&img, &grid[i][j], &grid[i][j + 1]);
+			if (grid[i + 1])
+				ft_draw_line(&img, &grid[i][j], &grid[i + 1][j]);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+}
+
+int	ft_init_mlx(void *mlx, t_data *img, void *mlx_window)
+{
+	mlx = mlx_init();
+	img.img = mlx_new_image(mlx, 700, 700);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.ll,
+	&img.endian);
+	mlx_window = mlx_new_window(mlx, 700, 700, "hi");
+	if ((!mlx) || (!mlx) || (!mlx_window))
+		return (0);
+	return (1);
+}
+
+int	ft_error_check(char **argv, int *fd)
+{
+	if (argc != 2)
+	{
+		printf("%s\n", "Wrong parameter specification. Use: ./fdf 'name of map file");
+		return (0);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		printf("%s\n", "Given file couldn't be opened. Check acces rights.");
+		return (0);
+	}
+	return (1);
 }
 
 int main(int argc, char **argv)
@@ -351,45 +404,22 @@ int main(int argc, char **argv)
 	void *mlx_window;
 	int		fd;
 	t_point	**grid;
-	int		i;
-	int		j;
 
-	i = 0;
-	j = 0;
-	if (argc != 2)
+	if (!ft_error_check(argv, &fd))
 		return (0);
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
+	if (!ft_init_mlx(mlx, &img, mlx_window))
+	{
+		printf("%s\n", "Error with the initialization of the minilibx. Pls try again.");
 		return (0);
-	mlx = mlx_init();
-	img.img = mlx_new_image(mlx, 700, 700);
-	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.ll,
-	&img.endian);
-	mlx_window = mlx_new_window(mlx, 700, 700, "hi");
+	}
 	grid = ft_initialize(fd);
 	if (!grid)
 	{
-		printf("map not properly formatted");
+		printf("Map not properly formatted.");
 		return (0);
 	}
-	while (grid[i])
-	{
-		while (grid[i][j].end)
-		{
-			if (grid[i][j + 1].end)
-				ft_draw_line(&img, &grid[i][j], &grid[i][j + 1]);
-			if (grid[i + 1])
-				ft_draw_line(&img, &grid[i][j], &grid[i + 1][j]);
-			// printf("x : %f y : %f z : %f height : %f max : %f\n", grid[i][j].x, grid[i][j].y, grid[i][j].z, grid[i][j].height, grid[i][j].max_height);
-			j++;
-		}
-		j = 0;
-		i++;
-		printf("\n");
-	}
-	i = 0;
-	j = 0;
-	// ft_free(grid);
+	ft_draw_grid(grid);
 	mlx_put_image_to_window(mlx, mlx_window, img.img, 0, 0);
+	//ft_free(grid, &img, mlx, mlx_window);
 	mlx_loop(mlx);
 }
